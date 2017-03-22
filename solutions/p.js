092885165a58dbfab6f45c43fd636e252c1c470d
whizzant <tim.fortney@gmail.com>
2017-03-22 13:46:10 -0400

    end of day

---
+++ solutions/p.js
@@ -0,0 +1,4 @@
+var problems = {
+    title: "Multiples of 3 and 5",
+    desc: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\nFind the sum of all the multiples of 3 or 5 below 1000.",
+}
