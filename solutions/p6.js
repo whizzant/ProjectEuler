0b35cd8c39f3cd33b7395a6df64aa02b088aa881
whizzant <tim.fortney@gmail.com>
2017-03-23 08:00:20 -0400

    Some changes to the interface, will keep playing with that. Solved problem 6 Created problem 7

---
+++ solutions/p6.js
@@ -0,0 +1,27 @@
+var p6 = {
+    title: "Sum square difference",
+    desc: 'The sum of the squares of the first ten natural numbers is,<br />'
+            + '12 + 22 + ... + 102 = 385<br />'
+            + '<br />'
+            + 'The square of the sum of the first ten natural numbers is,<br />'
+            + '(1 + 2 + ... + 10)2 = 552 = 3025<br />'
+            + '<br />'
+            + 'Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.<br />'
+            + '<br />'
+            + 'Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.<br />',
+    answer: function () {
+        var s1 = 0;
+        var s2 = 0;
+
+        for(i=1; i<=100; i++) {
+            s1 += Math.pow(i, 2);
+            s2 += i;
+        }
+
+        return Math.pow(s2, 2) - s1;
+    }
+};
+
+
+
+
---
+++ solutions/p7.js
@@ -0,0 +1,9 @@
+var p7 = {
+    title:"10001st prime",
+    desc:'By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.<br />'
+        + '<br />'
+        + 'What is the 10,001st prime number?',
+    answer: function() {
+        return "dont know"
+    }
+};
