4223118f7e09f547956a1bb42bfa9c7d84ebee09
whizzant <tim.fortney@gmail.cm>
2017-03-23 20:21:16 -0400

    Solved problem 8 Created problem 9

---
+++ solutions/p9.js
@@ -0,0 +1,12 @@
+var p9 = {
+    title:"Special Pythagorean triplet",
+    desc:"A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,<br />"
+        + "a2 + b2 = c2<br />"
+        + "For example, 32 + 42 = 9 + 16 = 25 = 52.<br />"
+        + "<br />"
+        + "There exists exactly one Pythagorean triplet for which a + b + c = 1000.<br/>"
+        + "Find the product abc.",
+    answer: function() {
+        return "don't know";
+    }
+};
