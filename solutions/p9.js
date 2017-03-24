7eda9d9c003457e2c7482856c7a6e682fd0ba471
whizzant <tim.fortney@gmail.com>
2017-03-24 12:21:47 -0400

    Perf tweak to problem 9

--- solutions/p9.js
+++ solutions/p9.js
@@ -8,19 +8,23 @@ var p9 = {
         + "Find the product abc.",
     answer: function() {
         var value = 0;
+        var tripletSumTarget = 1000;
 
         //Known rule:   a<b<c
 
-        for(var c=1000; c>0; c--) {
+        for(var c=tripletSumTarget; c>0; c--) {
             for(var b=(c-1); b>0; b--) {
+                if(c+b > tripletSumTarget) continue;
+
                 for(var a=(b-1); a>0; a--) {
-                    if(a+b+c == 1000) {
+                    if(a+b+c == tripletSumTarget) {
                         var a2 = Math.pow(a, 2);
                         var b2 = Math.pow(b, 2);
                         var c2 = Math.pow(c, 2);
 
                         if((a2+b2) == c2) {
                             value = a*b*c;
+                            break;
                         }
                     }
                 }
